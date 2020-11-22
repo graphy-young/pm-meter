14b6b9e1f3f42d79a28d395c7457ec3b53688c6a
Youngdae <youngdae@TaylorMac.local>
2020-11-22 16:13:06 +0900

    some variable names changed.

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import honeywell_hpma115s0 as hw
 import os
 
-a = hw.Honeywell(port="/dev/serial0", baud=9600)
+sensor = hw.Honeywell(port="/dev/serial0", baud=9600)
 
 try:
-    data = a.read()
+    data = sensor.read()
     print(data)
 except:
-        print('error')
+        print('error occured')
