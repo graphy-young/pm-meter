8483d82234c4a2741d29b0a186a560c1498ea93a
Youngdae <youngdae@TaylorMac.local>
2020-11-22 16:28:59 +0900

    Change printing data format to linux command

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import os
 sensor = hw.Honeywell(port="/dev/serial0", baud=9600)
 
 try:
-    data = sensor.read()
-    print(data)
+    timestamp, pm10, pm25 = sensor.read()
+    #print(data)
+    os.system('echo {timestamp}, {pm10}, {pm25}')
+    
 except:
         print('error occured')
