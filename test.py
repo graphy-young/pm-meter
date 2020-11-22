331bac66afbf6c6fb19a98486cd61a23f52adb48
Youngdae <youngdae@TaylorMac.local>
2020-11-22 16:33:44 +0900

    error modification

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import honeywell_hpma115s0 as hw
 import os
+import datetime
 
 sensor = hw.Honeywell(port="/dev/serial0", baud=9600)
 
 try:
-    timestamp, pm10, pm25 = sensor.read()
+    timestamp, pm10, pm25 = sensor.read().split(',')
     #print(data)
     os.system('echo {timestamp}, {pm10}, {pm25}')
     
