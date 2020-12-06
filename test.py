801aefe8238001b303e31a8e57e6af997e4eb614
Youngdae Heo <ck4ckei@gmail.com>
2020-12-06 20:44:17 +0900

    MySQL insertion added.

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
 import honeywell_hpma115s0 as hw
 import os
 import datetime
+import pymysql, keys
+
+def logger(*args): 
+    """
+    All parameters should be on string-type!
+    """
+    from datetime import datetime
+    print('[' + str(datetime.now()) + ']', str(' '.join(args)))
 
 sensor = hw.Honeywell(port="/dev/serial0", baud=9600)
 
-timestamp, pm10, pm25 = str(sensor.read()).split(',')
-print(timestamp)
-print(pm10)
-print(pm25)
-#os.system('echo {timestamp}, {pm10}, {pm25}')
+mysql = pymysql.connect(
+    host=keys.host, 
+    port=keys.port, 
+    user=keys.user, 
+    password=keys.password, 
+    database=keys.dbname)
+cursor = mysql.cursor()
+
+measuredDateime, pm10, pm25 = str(sensor.read()).split(',')
+logger('measuredDatetime: {measuredDatetime}, PM10: {PM10}, PM2.5: {PM25}')
+os.system('echo {measuredTime}, {pm10}, {pm25}')
