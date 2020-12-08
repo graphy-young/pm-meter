a0509b0f88cc3dcf643d828fbe3ec979cecff368
Youngdae Heo <ck4ckei@gmail.com>
2020-12-08 14:31:19 +0900

    logger added & modified

--- get_measurements.py
+++ get_measurements.py
@@ -3,40 +3,52 @@ import honeywell_hpma115s0 as hw
 import os, sys, datetime
 import pymysql, keys
 import pandas as pd
-
-
+from datetime import datetime
 
 ''' function definition  '''
-def logger(*args): 
+
+def logger(self, *args):
     """
     All parameters should be on string-type!
     """
-    from datetime import datetime
     print('[' + str(datetime.now()) + ']', str(' '.join(args)))
 
-def escape(*args):
-    logger(*args)
+def logError(stationCode, er, escapeMessage):
+
+        station_code = stationCode
+        update_at = str(datetime.now())
+        errors = str(er)
+        errorData = '{station_code}, {update_at}, 2, {errors}'
+
+    with open('error.csv', 'a') as f:
+        if os.path.isfile('error.csv'):
+            f.write('\n')
+        else:
+            pass
+        f.write(errorData)
+    
+    logger(escapeMessage)
     from sys import exit
     exit()
 
-
 # Connect to Honeywell HPMA115S0-XXX sensor
 try:
+
     sensor = hw.Honeywell(port="/dev/serial0", baud=9600)
-    logger('Connection to sensor established successfully')
+
 except Exception as e:
-    
-    escape('Sensor communication failed! ERROR:', str(e))
-    
+
+    logError(1, e, ('Sensor communication failed! ERROR: ' + str(e)))
+
+logger('Connection to sensor established successfully')
 
 # Get datetime & pollution data from the sensor
 try:
     measuredDateime, pm10, pm25 = str(sensor.read()).split(',')
-    logger('measuredDatetime: {measuredDatetime}, PM10: {PM10}, PM2.5: {PM25}')
-    os.system('echo {measuredTime}, {pm10}, {pm25}')
 except Exception as e:
     escape('Getting data from sensor failed. ERROR:', e)
-
+logger('measuredDatetime: {measuredDatetime}, PM10: {PM10}, PM2.5: {PM25}')
+os.system('echo {measuredTime}, {pm10}, {pm25}')
 
 
 ''' Codes '''
