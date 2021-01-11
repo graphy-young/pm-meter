e9f2c8a0d8f94ec81b32d325d9cbd05edcfa3f08
Youngdae Heo <ck4ckei@gmail.com>
2021-01-11 23:36:11 +0900

    x750 ups board supported

--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install -y libatlas-base-dev rdate vim rdate
+sudo apt-get install -y libatlas-base-dev rdate vim rdate python-smbus i2c-tools
 pip3 install -r requirements.txt
 sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+
+cd ~
+git clone https://github.com/graphy-young/raspmeasure.git
+
+git clone https://github.com/geekworm-com/x750.git
+sudo chmod +x ./x750/x750.sh
+sudo bash ./x750/x750.sh
+rm -rf ~/x750ups.py
