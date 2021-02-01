edda5d05ee77a99b691007073763d08491e2f9e2
Youngdae Heo <ck4ckei@gmail.com>
2021-02-01 11:33:02 +0900

    Feb 01 modification

--- get_measurements.py
+++ get_measurements.py
@@ -21,18 +21,24 @@ def getSerial():
     Extract serial from cpuinfo file
   """
   cpuSerial = "0000000000000000" # 16 bytes
+
   try:
     f = open('/proc/cpuinfo','r')
+
     for line in f:
-      if line[0:6]=='Serial':
-        cpuSerial = line[10:26]
+      if line[0:6]=='Serial': 
+          cpuSerial = line[10:26]
     f.close()
+
   except:
     cpuSerial = "UNKNOWN_SERIAL0"
+
   return cpuSerial
 
 def getStationCode():
-    # Get station code using RPi's CPU seiral code
+    """
+        Get station code using RPi's CPU seiral code
+    """
     try:
         connection = pymysql.connect(host=keys.host, port=keys.port, 
                                     user=keys.userName, password=keys.password, 
@@ -48,51 +54,72 @@ def getStationCode():
                 """
         cursor.execute(query)
         stationCode = cursor.fetchone()[0]
-        logger(f'Station code [{stationCode}] Fetched from DB successfully')
+        logger(f"Station code '{stationCode}' Fetched from DB successfully")
+
     except Exception as e:
         msg = f'Searching station code from DB failed! error: {[str(e)]}'
         logError(e, msg)
+
     finally:
         connection.close()
+
     return stationCode
 
 def logError(er, *args):
+    
     stationCode = getStationCode()
     updatedTime = str(datetime.now())
     errors = str(er)
     errorData = f'{stationCode}, {updatedTime}, 2, {errors}'
     escapeMessage = str(' '.join(args))
+
     try:
         connection = pymysql.connect(host=keys.host, port=keys.port, 
                                     user=keys.userName, password=keys.password, 
                                     database=keys.dbName)
         cursor = connection.cursor()
+
         eFileName = 'error.csv'
+        eTableName = 'command_log'
+        eColumnList = ['station_code', 'executed_time', 'file_descriptor', 'command']
+        eColumnList = ', '.join(eColumnList)
+
         if os.path.isfile(eFileName):
             logger('Found previous log that could not be saved properly to DB server. Try again to save those...')
+
             errorFile = pd.read_csv(eFileName, encoding='utf-8', header=None)
             errorFile = list(errorFile.to_records(index=False))
-            query = """INSERT INTO device_log (stationCode, updated_at, file_descriptor, command) 
-                        VALUES (%s, %s, %s, %s)"""
+
+            query = f"""
+                        INSERT INTO {eTableName} ({eColumnList}) 
+                        VALUES (%s, %s, %s, %s);
+                    """
             cursor.executemany(query, errorFile)
             connection.commit()
+
             if int(cursor.rowcount) > 1:
                 messageVerb = 'were'
             else:
                 messageVerb = 'was'
             logger('Previous', str(cursor.rowcount), 'log', messageVerb, 'inserted.')
-        query = f"""INSERT INTO device_log (station_code, updated_time, file_descriptor, command) 
-                    VALUES ({stationCode}, {updatedTime}, 2, {errors})"""
+        query = f"""
+                    INSERT INTO device_log (station_code, updated_time, file_descriptor, command) 
+                    VALUES ({stationCode}, {updatedTime}, 2, {errors})
+                """
         cursor.execute(query)
         connection.commit()
+
     except Exception as e:
         errorData = f'{stationCode}, {updatedTime}, 2, {e}'
+
         with open(eFileName, 'a', encoding='utf8') as f:
             if os.path.isfile(eFileName):
                 f.write('\n')
             f.write(errorData)
+
     finally:
         connection.close()
+
     logger(escapeMessage)
     from sys import exit
     exit()
@@ -101,29 +128,35 @@ def logError(er, *args):
 
 """ Codes """
 if __name__ == "__main__":
-    # Connect to Honeywell HPMA115S0-XXX sensor
+    # Connect to Honeywell HPMA115S0-XXX 
     try:
         sensor = hw.Honeywell(port="/dev/serial0", baud=9600)
         logger('Connection to sensor established successfully')
+
     except Exception as e:
         msg = ('Sensor communication failed! ERROR: ' + str(e))
         logError(e, msg)
+
 
     # Sync device's time via remote time server
     try:
         os.system('sudo rdate -s time.bora.net')
         logger('System time sync got successful')
+
     except Exception as e:
         msg = ('time sync got failed! Error: ' + str(e))
         logError(e, msg)
+
 
     # Get datetime & pollution data from the sensor
     try:
         measuredDatetime, pm10, pm25 = str(sensor.read()).split(',')
         logger(f'[DATA] measuredDatetime: {measuredDatetime}, PM10: {pm10}, PM25: {pm25}')
+
     except Exception as e:
         msg = 'Getting data from sensor failed. ERROR:' + str(e)
         logError(str(e), msg)
+
 
     try:
         connection = pymysql.connect(host=keys.host, port=keys.port, 
@@ -152,11 +185,14 @@ if __name__ == "__main__":
 
         query = f"""
                     INSERT INTO {mTableName} ({mColumnList})
-                    VALUES ('{getStationCode()}', {str(datetime.now())}, {pm10}, {pm25});
+                    VALUES ('{getStationCode()}', '{str(datetime.now())}'', {pm10}, {pm25});
                 """
         cursor.execute(query)
         connection.commit()
+        logger('Sending measurement process ended successfuly!')
+
     except Exception as e:
         logError(e, 'Sending measurements failed! ERROR: ' + str(e))
+
     finally:
         connection.close()
